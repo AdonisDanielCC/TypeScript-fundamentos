abcd9736606c0c31faffd5718bd2f8cbf574b843
Adonis Contreras <adoniscas@hotmail.com>
2022-02-24 15:53:49 -0600

    como declarar un arrary en typescript y insertar un dato al arreglo con push

---
+++ Proyecto/Seccion-2/array.js
@@ -0,0 +1,9 @@
+var mi_lista = [21, 23, 34];
+var mis_numeros = [1, 2, 3];
+mis_numeros.push(55);
+//mis_ numeros.push("pogba" ) nos genera un errror ya q el arreglo mis_numeros es de caracteres numericos
+var mi_equipo = ["casilla", "Buffon", "Messi"];
+mi_equipo.push("pogba");
+console.log(mi_equipo);
+console.log(mi_equipo.length);
+console.log(mi_equipo[2]);
--- Proyecto/Seccion-2/array.ts
+++ Proyecto/Seccion-2/array.ts
@@ -1,2 +1,12 @@
 let mi_lista= [21,23,34]
 let mis_numeros:number[]=[1,2,3]
+
+
+mis_numeros.push(55)
+//mis_ numeros.push("pogba" ) nos genera un errror ya q el arreglo mis_numeros es de caracteres numericos
+
+let mi_equipo: string[] = ["casilla", "Buffon", "Messi"]
+mi_equipo.push("pogba")
+console.log(mi_equipo)
+console.log(mi_equipo.length)
+console.log(mi_equipo[2])
