249334d188a7e6750cf69681c786067ffa59d7fd
Adonis Contreras <adoniscas@hotmail.com>
2022-02-25 15:12:33 -0600

    agregamos  void y any

---
+++ Proyecto/Seccion-2/any.ts
@@ -0,0 +1,14 @@
+let obj_libreria: any
+obj_libreria = 4;
+obj_libreria= true;
+obj_libreria="No la c"
+
+//podria tener atributos
+
+console.log(obj_libreria.atributo_desconicido)
+
+//podria tener metodos
+
+console.log(obj_libreria.metodo_desconocido())
+
+let lista: any[]= [20, true, false, "string"]
---
+++ Proyecto/Seccion-2/void.js
@@ -0,0 +1,9 @@
+var inutil = undefined;
+var vacio = null;
+var respuesta = "Datos cargados";
+if (respuesta != vacio) {
+    console.log("cargar pagina");
+}
+else {
+    console.log("error al cargar datos");
+}
