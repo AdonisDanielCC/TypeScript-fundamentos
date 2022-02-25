95177fe9a0acfa4566b7bc3d23e2e475c214b524
Adonis Contreras <adoniscas@hotmail.com>
2022-02-25 14:46:12 -0600

    tuplas en typeScript

---
+++ Proyecto/Seccion-2/tupla.js
@@ -0,0 +1,11 @@
+var vector;
+var vector2;
+vector = [3, 5];
+vector2 = [6, 12];
+//sumar vectores
+var vector_resultante = [vector[0] + vector2[0], vector[1] + vector2[1]];
+console.log(vector_resultante);
+var tupla_5_elementos = [1, 2, 3, 4, 5000];
+var profesor = ["Daniel Contreras", 1.90];
+var estudiante = ["su nombre", 1.80];
+console.log(profesor[0] + " mide " + estudiante[1] + " metros");
---
+++ Proyecto/Seccion-2/tupla.ts
@@ -0,0 +1,18 @@
+let vector: [number, number];
+let vector2:[number, number];
+
+vector =[3,5]
+vector2=[6,12]
+
+//sumar vectores
+
+let vector_resultante: [number, number] = [vector[0] + vector2[0],vector[1] + vector2[1]]
+
+console.log(vector_resultante)
+
+let tupla_5_elementos: [number, number, number, number, number, ]=[1,2,3,4,5000]
+
+let profesor: [string, number]=["Daniel Contreras", 1.90]
+let estudiante:[string, number]=["su nombre", 1.80]
+
+console.log(`${profesor[0]} mide ${estudiante[1]} metros`)
